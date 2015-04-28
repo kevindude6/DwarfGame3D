43c45c37c822e563a4eb14a46212bee353d4a69d
kevindude6 <kevin.dicola@gmail.com>
2015-04-27 22:52:48 -0400

    Added lock for pathfinding manager data
    should stop crashing when elements are removed from data list during
    processing

--- BradGame3D/BradGame3D/AI/Pathing/PathingManager.cs
+++ BradGame3D/BradGame3D/AI/Pathing/PathingManager.cs
@@ -18,18 +18,23 @@ namespace BradGame3D.AI.Pathing
 
         public static World2 w;
         public static List<PathData> data = new List<PathData>();
-        
+        private static Object myLock = new Object();
         
         public static void findPaths()
         {
+            
             while (true)
             {
                 if (data.Count() > 0)
                 {
-                    PathData p = data[0];
-                    p.b.followPath(Pathing.findPath(p.start, p.end, w));
-                    data.RemoveAt(0);
-                    Thread.Sleep(20);
+                    lock (myLock)
+                    {
+
+                        PathData p = data[0];
+                        p.b.followPath(Pathing.findPath(p.start, p.end, w));
+                        data.RemoveAt(0);
+                        Thread.Sleep(20);
+                    }
                 }
                 else
                     Thread.Sleep(50);
